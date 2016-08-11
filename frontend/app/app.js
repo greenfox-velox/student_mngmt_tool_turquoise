77205eeeb51790820372eba9f43e07b69fba2782
agyenes <aron.gyenes@gmail.com>
2016-08-11 13:36:47 +0200

    html and app.js modified to student theme

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,57 +1,61 @@
+var logger = require(./frontend-logger);
 var routerApp = angular.module('routerApp', ['ui.router']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
-
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
 
     .state('home', {
       url: '/',
-      templateUrl: 'partial-home.html'
+      templateUrl: 'partial-home.html',
+      controller: 'homeController'
     })
 
-    .state('home.list', {
-      url: '/list',
-      templateUrl: 'partial-home-list.html',
-      controller: function($scope) {
-        $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-      }
+    .state('home.teacher', {
+      url: '/teacher',
+      templateUrl: 'partial-home-list.html'
     })
 
-    .state('home.paragraph', {
-      url: '/paragraph',
-      template: 'I could sure use a drink right now.'
+    .state('home.student', {
+      url: '/student',
+      template: 'You are a student. Please input your name and password.'
     })
 
-    .state('about', {
-      url: '/about',
+    .state('student', {
+      url: '/student',
       views: {
         '': { templateUrl: 'partial-about.html' },
-        'columnOne@about': { template: 'Look I am a column!' },
-        'columnTwo@about': {
+        'columnOne@student': { template: 'The selected student is: ' },
+        'columnTwo@student': {
           templateUrl: 'table-data.html',
-          controller: 'scotchController'
+          controller: 'studentsController'
         }
       }
     });
 });
 
-routerApp.controller('scotchController', function($scope) {
-  $scope.message = 'test';
+routerApp.controller('homeController', ['$scope', '$http', function($scope, $http) {
+  logger('home', level, new Date());
+}]);
 
-  $scope.scotches = [
+routerApp.controller('studentsController', ['$scope', '$http', function($scope, $http) {
+  logger('students', level, new Date());
+  $scope.students = [
     {
-      name: 'Macallan 12',
-      price: 50
+      name: 'Shirinbekov Oleg',
+      scholarship: 'yes',
+      result: 40
     },
     {
-      name: 'Chivas Regal Royal Salute',
-      price: 10000
+      name: 'Galaschek Péter',
+      scholarship: 'no',
+      result: 38
     },
     {
-      name: 'Glenfiddich 1937',
-      price: 20000
+      name: 'Mbemba Jean-Claude',
+      scholarship: 'yes',
+      result: 43
     }
   ];
-});
+}]);
