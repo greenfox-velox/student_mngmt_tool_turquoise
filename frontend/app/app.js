42f5921ab1aaa287f1312f2f2d2b6f09bd96181d
agyenes <aron.gyenes@gmail.com>
2016-08-11 11:09:07 +0200

    basic ui router set up

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,12 +2,12 @@ var routerApp = angular.module('routerApp', ['ui.router']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/');
 
   $stateProvider
 
     .state('home', {
-      url: '/home',
+      url: '/',
       templateUrl: 'partial-home.html'
     })
 
@@ -22,5 +22,36 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     .state('home.paragraph', {
       url: '/paragraph',
       template: 'I could sure use a drink right now.'
+    })
+
+    .state('about', {
+      url: '/about',
+      views: {
+        '': { templateUrl: 'partial-about.html' },
+        'columnOne@about': { template: 'Look I am a column!' },
+        'columnTwo@about': {
+          templateUrl: 'table-data.html',
+          controller: 'scotchController'
+        }
+      }
     });
+});
+
+routerApp.controller('scotchController', function($scope) {
+  $scope.message = 'test';
+
+  $scope.scotches = [
+    {
+      name: 'Macallan 12',
+      price: 50
+    },
+    {
+      name: 'Chivas Regal Royal Salute',
+      price: 10000
+    },
+    {
+      name: 'Glenfiddich 1937',
+      price: 20000
+    }
+  ];
 });
