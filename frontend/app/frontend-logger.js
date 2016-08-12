4331715ccb71c895156ac58db7ad47ff62ecbdb1
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-08-12 11:09:56 +0200

    refactor frontend logger

--- frontend/app/frontend-logger.js
+++ frontend/app/frontend-logger.js
@@ -1,8 +1,31 @@
 'use strict';
+var managementApp = angular.module("managementApp");
+var logLevel = 1;
 
-var logger = function(debugLogText, level, date) {
-    $http.post('/api/log', { level: level, debugLogText: debugLogText, date: date, location: 'frontend' })
-      .success(function(data) {});
+managementApp.factory("logger", function($http) {
+  debug: function(text) {
+    if (logLevel <= 0) {
+      loggerPost(text, 'debug');
+    }
+  }
+  info: function(text) {
+    if (logLevel <= 1) {
+      loggerPost(text, 'info');
+    }
+  }
+  warn: function(text) {
+    if (logLevel <= 2) {
+      loggerPost(text, 'warn');
+    }
+  }
+  error: function(text) {
+    if (logLevel <= 3) {
+      loggerPost(text, 'error');
+    }
+  }
+}
+
+var loggerPost = function(debugLogText, level) {
+  $http.post('/api/log', { level: level, debugLogText: debugLogText, date: new date(), location: 'frontend' })
+    .success(function(data) {});
 };
-
-module.exports = logger;
