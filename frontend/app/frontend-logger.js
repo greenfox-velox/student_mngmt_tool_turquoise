830ba23515adfa0d6ce84245fae6f55ae660b770
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-08-12 14:15:34 +0200

    refactor frontend logger

--- frontend/app/frontend-logger.js
+++ frontend/app/frontend-logger.js
@@ -3,29 +3,30 @@ var managementApp = angular.module("managementApp");
 var logLevel = 1;
 
 managementApp.factory("logger", function($http) {
-  debug: function(text) {
-    if (logLevel <= 0) {
-      loggerPost(text, 'debug');
-    }
-  }
-  info: function(text) {
-    if (logLevel <= 1) {
-      loggerPost(text, 'info');
-    }
-  }
-  warn: function(text) {
-    if (logLevel <= 2) {
-      loggerPost(text, 'warn');
-    }
-  }
-  error: function(text) {
-    if (logLevel <= 3) {
-      loggerPost(text, 'error');
-    }
-  }
-}
-
-var loggerPost = function(debugLogText, level) {
-  $http.post('/api/log', { level: level, debugLogText: debugLogText, date: new date(), location: 'frontend' })
+  var loggerPost = function(debugLogText, level, date) {
+    $http.post('/api/log', { level: level, debugLogText: debugLogText, date: new date(), location: 'frontend' })
     .success(function(data) {});
-};
+  };
+  return {
+    debug: function(text) {
+      if (logLevel <= 0) {
+        loggerPost(text, 'debug');
+      }
+    },
+    info: function(text) {
+      if (logLevel <= 1) {
+        loggerPost(text, 'info');
+      }
+    },
+    warn: function(text) {
+      if (logLevel <= 2) {
+        loggerPost(text, 'warn');
+      }
+    },
+    error: function(text) {
+      if (logLevel <= 3) {
+        loggerPost(text, 'error');
+      }
+    }
+  };
+});
