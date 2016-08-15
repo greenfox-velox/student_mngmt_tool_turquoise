4af4e0da375104ca363f98b4968a10cb022d975d
Pfeter <peter.foldvary@gmail.com>
2016-08-15 15:06:31 +0200

    Add api/log endpoint

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 'use strict';
+
 var db = require('./db');
 var express = require('express');
 var bodyParser = require('body-parser');
+var logger = require('./backend_logger')();
 
 function newApp(connection) {
   var app = express();
-
   app.use(bodyParser.json());
   app.use(express.static('frontend'));
   app.use(function(req, res, next) {
@@ -25,7 +26,25 @@ function newApp(connection) {
         }
       });
   });
-
+  app.post('/api/log', function(req, res) {
+    switch (req.body.level) {
+      case 'debug':
+        logger.debug(req.body.text, req.body.date);
+        break;
+      case 'info':
+        logger.info(req.body.text, req.body.date);
+        break;
+      case 'warn':
+        logger.warn(req.body.text, req.body.date);
+        break;
+      case 'error':
+        logger.error(req.body.text, req.body.date);
+        break;
+      default:
+        logger.info(req.body.text);
+        break;
+    }
+  });
   return app;
 }
 
