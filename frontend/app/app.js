40668f15c9a8fc9d6ffa57e069af1c3b85401fc6
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-08-17 22:22:53 +0200

    seperate register controller from app.js

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,3 +1,4 @@
+'use strict';
 var managementApp = angular.module('managementApp', ['ui.router']);
 
 managementApp.config(function($stateProvider, $urlRouterProvider) {
@@ -31,36 +32,4 @@ managementApp.config(function($stateProvider, $urlRouterProvider) {
 
 managementApp.controller('homeController', ['$scope', '$http', 'logger', function($scope, $http, logger) {
   logger.info('home controller');
-}]);
-
-managementApp.controller('registerController', ['$scope', '$http', '$state', '$location', 'logger', function($scope, $http, $state, $location, logger) {
-  logger.info('register controller');
-
-  function getUrl() {
-    return ($location.absUrl().split('/#/')[0]);
-  }
-
-  function clearInputFields() {
-    $scope.newMember.email = '';
-    $scope.newMember.password = '';
-    $scope.newMember.confirmPassword = '';
-  }
-
-  function newMemberMaker() {
-    return { email: $scope.newMember.email, password: $scope.newMember.password };
-  }
-
-  $scope.submitRegister = function() {
-    if ($scope.newMember.password === $scope.newMember.confirmPassword) {
-      $http.post(getUrl() + '/api/register', newMemberMaker())
-        .then(function successCallback(response) {
-          $state.go('your');
-        }, function errorCallback(response) {
-          $scope.errorMsg = 'Registration error: e-mail address is not valid or already exist';
-        });
-    } else {
-      $scope.errorMsg = 'Registration error: confirmed password does not match original';
-    }
-    clearInputFields();
-  };
 }]);
