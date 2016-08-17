34ba24713b0ff8de4c31160210d3fcc7eb7d0a30
agyenes <aron.gyenes@gmail.com>
2016-08-17 13:41:34 +0200

    login api in server js

--- server.js
+++ server.js
@@ -48,6 +48,16 @@ function newApp(connection) {
     res.send(200);
   });
 
+  app.post('/api/login', function(req, res) {
+    myDataBase.loginUser(req.body, function(err, result) {
+      if (!err) {
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(500);
+      }
+    });
+  });
+
   app.post('/api/register', function(req, res) {
     myDataBase.registerNewUser(req.body, function(err, result) {
       if (!err) {
