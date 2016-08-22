550452b11b4655b629b546f16e203751afa79f49
agyenes <aron.gyenes@gmail.com>
2016-08-22 10:49:16 +0200

    login controller in separate file

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,4 +1,7 @@
 'use strict';
+
+var yourUrl = 'http://localhost:3000';
+var yourUser = 'peter@email.com';
 var managementApp = angular.module('managementApp', ['ui.router']);
 
 managementApp.config(function($stateProvider, $urlRouterProvider) {
@@ -33,32 +36,8 @@ managementApp.config(function($stateProvider, $urlRouterProvider) {
 
 managementApp.controller('homeController', ['$scope', '$http', 'logger', function($scope, $http, logger) {
   logger.info('home controller');
-}]);
-
-managementApp.controller('loginController', ['$scope', '$http', '$state', '$location', 'logger', function($scope, $http, $state, $location, logger) {
-  logger.info('login controller');
-
-  function getUrl() {
-    return ($location.absUrl().split('/#/')[0]);
-  }
-
-  function clearInputFields() {
-    $scope.loggedinMember.email = '';
-    $scope.loggedinMember.password = '';
-    $scope.errorMsg = '';
-  }
-
-  function memberLogin() {
-    return { email: $scope.loggedinMember.email, password: $scope.loggedinMember.password };
-  }
-
-  $scope.submitLogin = function() {
-    $http.post(getUrl() + '/api/login', memberLogin())
-      .then(function successCallback(response) {
-        $state.go('your');
-      }, function errorCallback(response) {
-        $scope.errorMsg = 'Login error: e-mail and password do not match';
-      });
-    clearInputFields();
-  };
+  $http.get(yourUrl + '/your/' + yourUser).success(function(yourData) {
+    logger.info('yourController http get');
+    $scope.showYourData(yourData);
+  });
 }]);
