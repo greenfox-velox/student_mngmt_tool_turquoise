d6393a8b081b849b12ec8157ff7251230eeffa0e
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-09-05 11:23:07 +0200

    add backend directory

--- test/test_backend_server.js
+++ test/test_backend_server.js
@@ -28,7 +28,7 @@ describe('GET /heartbeat', function() {
       });
   });
 });
-0
+
 describe('POST /api/log', function() {
   it('respond with 200', function(done) {
     var app = newApp(mockConnections.mockConnection);
@@ -41,3 +41,43 @@ describe('POST /api/log', function() {
     });
   });
 });
+
+describe('POST /api/register', function() {
+  it('respond with 200', function(done) {
+    var app = newApp(mockConnections.mockConnection);
+    supertest(app)
+      .post('/api/register')
+      // .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('respond with 500', function(done) {
+    var app = newApp(mockConnections.mockConnectionError);
+    supertest(app)
+      .post('/api/register')
+      .expect(500)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+// app.post('/api/register', function(req, res) {
+//   studentDataBase.registerNewUser(req.body, function(err, result) {
+//     if (err) {
+//       res.sendStatus(500);
+//       return;
+//     }
+//     req.login({id: result.insertId}, function(error) {
+//       if (error) {
+//         logger.error(error);
+//         return res.sendStatus(500);
+//       }
+//       res.sendStatus(200);
+//     });
+//   });
+// });
