686f99ebc4742fb2001ce1f534140ad7a8e2d201
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-09-08 11:56:56 +0200

    add another test for register controller

--- test/unit/test-app-register-controller.js
+++ test/unit/test-app-register-controller.js
@@ -11,8 +11,8 @@ describe('test-app-register-controller', function() {
     $httpBackend = $injector.get('$httpBackend');
     $rootScope = $injector.get('$rootScope');
     $location = $injector.get('$location');
-    $httpBackend.when('POST', getUrl($location) + '/api/register', { email: 'asd', password: 'asd' })
-    .respond(500);
+    $httpBackend.when('POST', getUrl($location) + '/api/register', '{"email":"asd","password":"asd"}')
+      .respond(500);
     $httpBackend.when('POST', getUrl($location) + '/api/register')
       .respond(200);
     $httpBackend.when('POST', 'https://student-mngmt-tool.herokuapp.com/api/log')
@@ -39,13 +39,13 @@ describe('test-app-register-controller', function() {
     scope.newMember.confirmPassword = 'asd';
     scope.newMember.email = 'asd@asd.as';
     scope.submitRegister();
+    $httpBackend.flush();
     expect(scope.newMember).toBeDefined();
     expect(scope.newMember.password).toBe('');
     expect(scope.newMember.confirmPassword).toBe('');
     expect(scope.newMember.email).toBe('');
     expect(scope.newMember.password).toBe('');
     expect(scope.errorMsg).toBe('');
-    $httpBackend.flush();
   });
 
   it('should test register controller with password and confirmPassword do not match', function() {
@@ -56,12 +56,30 @@ describe('test-app-register-controller', function() {
     scope.newMember.confirmPassword = 'asdq';
     scope.newMember.email = 'asd@asd.as';
     scope.submitRegister();
+    $httpBackend.flush();
     expect(scope.newMember).toBeDefined();
     expect(scope.newMember.password).toBe('');
     expect(scope.newMember.confirmPassword).toBe('');
     expect(scope.newMember.email).toBe('');
     expect(scope.newMember.password).toBe('');
     expect(scope.errorMsg).toBe('Registration error: confirmed password does not match original');
+  });
+
+  it('should test register controller with password and confirmPassword do not match', function() {
+    $httpBackend.expectPOST(getUrl($location) + '/api/register', { email: 'asd', password: 'asd' });
+    createController();
+    scope.errorMsg = '';
+    scope.newMember = {};
+    scope.newMember.password = 'asd';
+    scope.newMember.confirmPassword = 'asd';
+    scope.newMember.email = 'asd';
+    scope.submitRegister();
     $httpBackend.flush();
+    expect(scope.newMember).toBeDefined();
+    expect(scope.newMember.password).toBe('');
+    expect(scope.newMember.confirmPassword).toBe('');
+    expect(scope.newMember.email).toBe('');
+    expect(scope.newMember.password).toBe('');
+    expect(scope.errorMsg).toBe('Registration error: e-mail address is not valid or already exist');
   });
 });
